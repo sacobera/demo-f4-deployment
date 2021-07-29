69182ac169ead493cc5c01faea3ebc3948dcfd8b
Sarah Acobera <sacobera1992@go.byuh.edu>
2021-07-29 08:24:52 -1000

    added endpoint get

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path');
 
-const path = require('path') //creates a path that joins all the other paths 
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, '../index.html'))
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 5500
 
-app.listen(port, () => {console.log('Listening on port ${port}')})
-
+app.listen(port ,() => { console.log(`Listening on port ${port}`)})
