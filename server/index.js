ef468f41aba7c97b5bfbea349a0ae05f61ebbd2a
Sarah Acobera <sacobera1992@go.byuh.edu>
2021-07-29 08:18:18 -1000

    added endpoint get

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const path = require('path') //creates a path that joins all the other paths
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, '../index.html'))
+})
+
 const port = process.env.PORT || 5500
 
 app.listen(port, () => {console.log('Listening on port ${port}')})
